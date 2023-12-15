3fbf7eca9e10ad2664ab0a6e3849232bb31a3056
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-12-15 14:26:26 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,66 @@
+    data = generate_random_data()
+
+if __name__ == "__main__":
+def main():
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+    return data
+def generate_random_data():
+
+
+        print(f"Random Number: {item}")
+    main()
+    return data
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+if __name__ == "__main__":
+    for item in data:
+
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
+    main()
+
+    return data
+
+if __name__ == "__main__":
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    for item in data:        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+if __name__ == "__main__":
+
+
+    main()
+    for item in data:
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return data
+def generate_random_data():    data = generate_random_data()
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def main():
+
+    return data
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+import random
